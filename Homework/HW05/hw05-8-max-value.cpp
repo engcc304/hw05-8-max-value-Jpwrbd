1459e25d1664eb5bc6493eb36883cbcb67022e7f
Jpwrbd <jpwrbd456@gmail.com>
2023-08-10 20:03:28 +0700

    up

--- Homework/HW05/hw05-8-max-value.cpp
+++ Homework/HW05/hw05-8-max-value.cpp
@@ -20,3 +20,33 @@
         The 1rd Number is the greatest among three
 
 */
+
+#include <stdio.h>
+int main()
+{
+    int a1 = 0, a2 = 0, a3 = 0;
+    printf("Input[1]: ");
+    scanf("%d", &a1);
+    printf("Input[2]: ");
+    scanf("%d", &a2);
+    printf("Input[3]: ");
+    scanf("%d", &a3);
+    printf("Output: ");
+    if (a1 > a2 && a1 > a3)
+    {
+        printf("1st Number = %d ", a1);
+        if (a2 > a3)
+            printf("1st Number = %d %d ", a2, a3);
+        else
+            printf("1st Number = %d %d ", a2, a3);
+    }
+    else if (a2 > a1 && a2 > a3)
+    {
+        printf("%d ", a2);
+        if (a1 > a3)
+            printf("%d %d ", a1, a3);
+        else
+            printf("%d %d ", a3, a1);
+    }
+    return 0;
+}
